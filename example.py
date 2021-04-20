4892d55d78a4d331b5f4d71dffeea6bae78b58a5
luigifaticoso <faticosoluigi@gmail.com>
2021-04-20 18:43:46 +0200

    modified example to work with custom Video and progress Bar

--- example.py
+++ example.py
@@ -6,9 +6,12 @@ Check the README.md for complete documentation.
 import cv2
 from gaze_tracking import GazeTracking
 
-gaze = GazeTracking()
-webcam = cv2.VideoCapture(0)
 
+gaze = GazeTracking()
+webcam = cv2.VideoCapture('input.mkv')
+length = int(webcam.get(cv2.CAP_PROP_FRAME_COUNT))
+out = cv2.VideoWriter('output.avi', -1, 20.0, (640,480))
+count = 0
 while True:
     # We get a new frame from the webcam
     _, frame = webcam.read()
@@ -35,7 +38,14 @@ while True:
     cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
     cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
 
-    cv2.imshow("Demo", frame)
-
-    if cv2.waitKey(1) == 27:
+    # cv2.imshow("Demo", frame)
+    out.write(frame)
+    print("\t"+str(count)+"/"+str(length), end = "\r")
+    count += 1
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+webcam.release()
+out.release()
+cv2.destroyAllWindows()
+
